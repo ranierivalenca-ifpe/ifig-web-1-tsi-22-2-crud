5e82c44e080577da9838d088f9de083f39f35cbc
ranierivalenca <ranierivalenca@gmail.com>
2022-12-16 11:01:48 -0300

    Conectando back-end ao SGBD

--- back/add-user.php
+++ back/add-user.php
@@ -1,4 +1,15 @@
 <?php
     header('Access-Control-Allow-Origin: *');
-    echo "Hello";
+
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    
+    $dsn = 'mysql:dbname=web1tsi;host=127.0.0.1;port=3307';
+    $conn = new PDO($dsn, 'root', 'root');
+
+    // $conn->query("INSERT INTO users(username, password) VALUES ('$username', '$password')");
+    $stmt = $conn->prepare('INSERT INTO users(username, password) VALUES (?, ?)');
+    $stmt->execute([$username, $password]);
+
+    http_response_code(201);
 ?>
