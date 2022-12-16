85505bb7b819fda8c1cbb238a71ab511a5d174f3
ranierivalenca <ranierivalenca@gmail.com>
2022-12-16 10:30:18 -0300

    Adicionando formulário de registro (front-end apenas)

--- front/src/lib/Register.svelte
+++ front/src/lib/Register.svelte
@@ -1,1 +1,39 @@
+<script>
+
+let username = '';
+let password = '';
+let password2 = '';
+
+function handle() {
+    // aqui vamos submeter o formulário
+    if (password != password2) return;
+
+    console.log('to aqui', {
+        username,
+        password,
+        password2
+    })
+}
+
+</script>
+
 <h1>Registro de usuário</h1>
+<form on:submit|preventDefault={handle}>
+    <input type="text" required bind:value={username} placeholder="home de usuário">
+    <input type="password" required bind:value={password} placeholder="senha">
+    <input type="password" required bind:value={password2} placeholder="confirme a senha">
+    {#if password != '' && password != password2}
+        <div class="danger">Senhas não conferem</div>
+    {/if}
+    <button>Cadastrar!</button>
+</form>
+
+<style>
+    form {
+        border: 1px solid gray;
+        padding: 1em;
+    }
+    .danger {
+        color: red;
+    }
+</style>
