001d13fe3af60f538b132257400a488cb967216d
ranierivalenca <ranierivalenca@gmail.com>
2022-12-16 11:39:56 -0300

    Logout no back-end

---
+++ back/logout.php
@@ -0,0 +1,6 @@
+<?php 
+
+    include 'main.php';
+    session_destroy();
+
+?>
--- front/src/lib/Navbar.svelte
+++ front/src/lib/Navbar.svelte
@@ -1,24 +1,19 @@
 <script>
   import { currentPage, isLogged } from "../stores";
 
+  function logout() {
+    fetch("http://localhost:8000/logout.php", {
+      credentials: "include",
+    });
+    $isLogged = false;
+  }
 </script>
 
-
-<button on:click={() => $currentPage = 'index'}>
-    Index
-</button>
+<button on:click={() => ($currentPage = "index")}> Index </button>
 {#if !$isLogged}
-    <button on:click={() => $currentPage = 'register'}>
-        Register
-    </button>
-    <button on:click={() => $currentPage = 'login'}>
-        Login
-    </button>
+  <button on:click={() => ($currentPage = "register")}> Register </button>
+  <button on:click={() => ($currentPage = "login")}> Login </button>
 {:else}
-    <button on:click={() => $currentPage = 'home'}>
-        Página inicial
-    </button>
-    <button on:click={() => $isLogged = false}>
-        Sair
-    </button>
+  <button on:click={() => ($currentPage = "home")}> Página inicial </button>
+  <button on:click={logout}> Sair </button>
 {/if}
