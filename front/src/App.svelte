954ef1b327a6c1c7659480bc7c48292990f35d6d
ranierivalenca <ranierivalenca@gmail.com>
2022-12-16 10:12:48 -0300

    Adicionando o componente Index

--- front/src/App.svelte
+++ front/src/App.svelte
@@ -1,5 +1,14 @@
 <script>
+  import Index from "./lib/Index.svelte";
+  import { currentPage } from "./stores";
+
 </script>
 
 <main>
+  <h1>Hello cruel world</h1>
+
+  {#if $currentPage == 'index'}
+    <Index />
+  {/if}
+
 </main>
